7ff0b7a5f97c60a25b3658c5750806ecea8236a6
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-12-06 15:31:14 +0100

    feat: basic layout

--- src/src/pages/MainPage/Main.tsx
+++ src/src/pages/MainPage/Main.tsx
@@ -1,15 +1,68 @@
-import { useThemeStore } from "../../zustand/useThemeStore"
+import { Box, InputAdornment, Stack, TextField } from '@mui/material';
+import { useThemeStore } from '../../zustand/useThemeStore';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { Logo } from '../../components/logo';
 
 export const MainPage: React.FC = () => {
-    //const {theme} = useThemeStore();
-    return (
-        <div 
-        style={{
-        minHeight: '100vh', 
-        padding: '1rem'}
-        }>
-            <h1>Welcome to the Main Page</h1>
-            <p>This is a sample main page using the current theme.</p>
-        </div>
-    )
-}
+  const { theme } = useThemeStore();
+  return (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        alignSelf: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          height: '33%',
+          minWidth: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          justifyItems: 'center',
+          display: 'flex',
+        }}
+      >
+        <Stack
+          component="div"
+          direction="row"
+          spacing={8}
+          alignItems="center"
+          width="100%"
+          height="100%"
+          sx={{ backgroundColor: theme.palette.background.paper, padding: 16 }}
+        >
+          <Box>
+            <Logo
+              style={{ width: '300px', height: '300px' }}
+              monochrome={false}
+            />
+          </Box>
+          <TextField
+            fullWidth
+            placeholder="Search for anything..."
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon sx={{ fontSize: '2.5rem' }} />
+                </InputAdornment>
+              ),
+              sx: {
+                // "BIG"
+                fontSize: '2rem',
+                // "Properly Rounded"
+                borderRadius: '50px',
+                // Adjust internal padding for height
+                '& .MuiOutlinedInput-input': {
+                  padding: '18px 0',
+                },
+              },
+            }}
+          />
+        </Stack>
+      </Box>
+    </div>
+  );
+};
