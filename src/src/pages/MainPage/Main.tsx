ff778adc3f421d2f5036ade0acae89fe3f5738cd
kurama <paul.zenker.pz@gmail.com>
2025-12-07 19:33:58 +0100

    chore: try adding an animation

--- src/src/pages/MainPage/Main.tsx
+++ src/src/pages/MainPage/Main.tsx
@@ -1,11 +1,17 @@
 import { Box, InputAdornment, Stack, TextField } from '@mui/material';
 import { useThemeStore } from '../../zustand/useThemeStore';
 import SearchIcon from '@mui/icons-material/Search';
+import { AnimatePresence, motion } from 'framer-motion';
+
 
 import { Logo } from '../../components/logo';
+import { useState } from 'react';
 
 export const MainPage: React.FC = () => {
   const { theme } = useThemeStore();
+  const [searchText, setSearchText] = useState('');
+  const hasText = searchText.length > 0;
+
   return (
     <div
       style={{
@@ -15,8 +21,21 @@ export const MainPage: React.FC = () => {
       }}
     >
       <Box
+        component={motion.div}
+        animate={{
+          height: hasText ? '15%' : '33%',
+          width: hasText ? '66vw' : '100vw',
+          position: 'absolute',
+          //top: hasText ? 0 : 'auto',
+          //left: hasText ? '50%' : 'auto',
+          //x: hasText ? '-50%' : 0,
+          zIndex: hasText ? 1000 : 1,
+        }}
+        transition={{
+          duration: 0.5,
+          ease: 'easeInOut',
+        }}
         sx={{
-          height: '33%',
           minWidth: '100%',
           alignItems: 'center',
           justifyContent: 'center',
@@ -33,17 +52,29 @@ export const MainPage: React.FC = () => {
           height="100%"
           sx={{ backgroundColor: theme.palette.background.paper, padding: 16 }}
         >
-          <Box>
-            <Logo
-              style={{ width: '300px', height: '300px' }}
-              monochrome={false}
-            />
-          </Box>
+
+            <AnimatePresence>
+              {!hasText && 
+              <Box 
+              component={motion.div} 
+              initial={{ opacity: 1 }} 
+              exit={{opacity: 0}} 
+              transition={{ duration: 0.3 }} sx={{ height: '33vh'}}>
+                <Logo
+                //style={{ width: '100%', height: '100%' }}
+                monochrome={false}
+              />
+              </Box>
+              }
+
+            </AnimatePresence>
           <TextField
             fullWidth
             placeholder="Search for anything..."
             variant="outlined"
-            InputProps={{
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            slotProps={{input: {
               startAdornment: (
                 <InputAdornment position="start">
                   <SearchIcon sx={{ fontSize: '2.5rem' }} />
@@ -59,7 +90,7 @@ export const MainPage: React.FC = () => {
                   padding: '18px 0',
                 },
               },
-            }}
+            }}}
           />
         </Stack>
       </Box>
