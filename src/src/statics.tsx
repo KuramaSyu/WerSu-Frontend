da25ab1de5ff8256ff01cfc860728216bb7b2e30
kurama <52566613+KuramaSyu@users.noreply.github.com>
2025-12-10 21:06:39 +0100

    chore: created statics for Margin and Paddings

--- src/src/statics.tsx
+++ src/src/statics.tsx
@@ -9,3 +9,11 @@ declare global {
 }
 
 export const BACKEND_BASE = import.meta.env.VITE_BACKEND_URL ?? '';
+export const M1 = '0.25rem';
+export const M2 = '0.5rem';
+export const M3 = '1rem';
+export const M4 = '2rem';
+export const M5 = '4rem';
+export const M6 = '8rem';
+export const M7 = '12rem';
+export const M8 = '16rem';
