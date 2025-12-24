a43d6225d5232b2ad6c798413acf587c91010fd8
kurama <paul.zenker.pz@gmail.com>
2025-12-24 22:39:49 +0100

    ui: wrap MainContent with a LoadingPage

--- src/src/pages/MainPage/Main.tsx
+++ src/src/pages/MainPage/Main.tsx
@@ -1,4 +1,4 @@
-import { Box, ButtonBase, Grid, InputAdornment, Paper, Stack, TextField } from '@mui/material';
+import { Box, ButtonBase, Grid, InputAdornment, Paper, Stack, TextField, ThemeProvider } from '@mui/material';
 import { useThemeStore } from '../../zustand/useThemeStore';
 import { AnimatePresence, motion } from 'framer-motion';
 
@@ -11,12 +11,23 @@ import { TestSearchNotesApi, type SearchNotesApi } from '../../api/SearchNotesAp
 import { useNotesStore } from '../../zustand/useNotesStore';
 import { NoteCard } from './NoteCard';
 import { M2, M3, M4, M5, M6 } from '../../statics';
+import { useUserStore } from '../../zustand/userStore';
+import { useLoadingStore } from '../../zustand/loadingStore';
+import { LoadingPage } from '../LoadingPage/Main';
+import { useBreakpoint } from '../../hooks/useBreakpoint';
+import { LoginPage } from '../LoginPage/Main';
 
 export const MainPage: React.FC = () => {
   const { theme } = useThemeStore();
   const [searchText, setSearchText] = useState('');
   const { notes, setNotes } = useNotesStore();
-  
+  const {user} = useUserStore();
+  const {isLoading} = useLoadingStore();
+  const { isMobile } = useBreakpoint();
+  const [exitPercentage, setExitPercentage] = useState(
+    Math.round(Math.random() * 100)
+  );
+  const oneOrZero = Math.round(exitPercentage / 100) * 100;
  
 
   // fetch notes
@@ -36,8 +47,8 @@ export const MainPage: React.FC = () => {
     ))
   }, [notes])
 
-  return (
-    <Box
+  const MainContent = () => (
+      <Box
       sx={{
         height: '100%',
         width: '100%',
@@ -57,6 +68,64 @@ export const MainPage: React.FC = () => {
       
       </Box>
     </Box>
+  );
+
+    return (
+    <ThemeProvider theme={theme}>
+      {/* Loading Animation for Loading Page
+      which is as long visible as "overlay" as
+      it needs to fetch all resources via REST. */}
+      <AnimatePresence initial={false}>
+        {isLoading && (
+          <motion.div
+            initial={false}
+            animate={{ clipPath: 'circle(100% at 50% 50%)' }}
+            exit={{
+              clipPath: oneOrZero
+                ? `circle(0% at 100% ${exitPercentage}%)`
+                : `circle(0% at ${exitPercentage}% 100%)`,
+              opacity: 0.2,
+            }}
+            transition={{
+              duration: 1,
+              ease: [0.4, 0, 0.2, 1],
+            }}
+            style={{
+              position: 'fixed',
+              zIndex: 9999,
+              top: 0,
+              left: 0,
+              width: '100vw',
+              height: '100vh',
+            }}
+          >
+            <LoadingPage></LoadingPage>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* Box for either the Main App or Login Page, depending on user state */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          height: '100%',
+          overflow: 'hidden', // Prevents overflow
+          paddingTop: user !== null && !isMobile ? '6px' : undefined,
+        }}
+      >
+        {user !== null || isLoading ? (
+          <>
+            <MainContent></MainContent>
+          </>
+        ) : (
+          <>
+            {console.log('render login page')}
+            <LoginPage></LoginPage>
+          </>
+        )}
+      </Box>
+    </ThemeProvider>
   );
 };
 
