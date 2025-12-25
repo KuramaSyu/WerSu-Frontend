d25681c3a3225b6cad748271e8e604f4ad352a69
kurama <paul.zenker.pz@gmail.com>
2025-12-26 00:03:24 +0100

    feat: connected create note to backend

--- src/src/pages/MainPage/Main.tsx
+++ src/src/pages/MainPage/Main.tsx
@@ -1,14 +1,28 @@
-import { Box, ButtonBase, Grid, InputAdornment, Paper, Stack, TextField, ThemeProvider } from '@mui/material';
+import {
+  Box,
+  ButtonBase,
+  Grid,
+  InputAdornment,
+  Paper,
+  Slide,
+  Snackbar,
+  Stack,
+  TextField,
+  ThemeProvider,
+} from '@mui/material';
 import { useThemeStore } from '../../zustand/useThemeStore';
 import { AnimatePresence, motion } from 'framer-motion';
-
 
 import { Logo } from '../../components/logo';
 import TopBar from '../../components/TopBar';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import CreateIcon from '@mui/icons-material/Create';
-import { TestSearchNotesApi, type SearchNotesApi } from '../../api/SearchNotesApi';
-import { useNotesStore } from '../../zustand/useNotesStore';
+import {
+  SearchNotesApi,
+  TestSearchNotesApi,
+  type ISearchNotesApi,
+} from '../../api/SearchNotesApi';
+import { useSearchNotesStore } from '../../zustand/useSearchNotesStore';
 import { NoteCard } from './NoteCard';
 import { M1, M2, M3, M4, M5, M6 } from '../../statics';
 import { useUserStore } from '../../zustand/userStore';
@@ -16,61 +30,83 @@ import { useLoadingStore } from '../../zustand/loadingStore';
 import { LoadingPage } from '../LoadingPage/Main';
 import { useBreakpoint } from '../../hooks/useBreakpoint';
 import { LoginPage } from '../LoginPage/Main';
+import { RestNotesSearchType } from '../../api/models/search';
+import { note_of_date_at_hour } from '../../utils/NoteTitleTemplates';
+import { NoteApi } from '../../api/NoteApi';
+import { useNotesStore } from '../../zustand/useNotesStore';
 
 export const MainPage: React.FC = () => {
   const { theme } = useThemeStore();
   const [searchText, setSearchText] = useState('');
-  const { notes, setNotes } = useNotesStore();
-  const {user} = useUserStore();
-  const {isLoading} = useLoadingStore();
+  const { notes, setNotes } = useSearchNotesStore();
+  const { user } = useUserStore();
+  const { isLoading } = useLoadingStore();
   const { isMobile } = useBreakpoint();
   const [exitPercentage, setExitPercentage] = useState(
     Math.round(Math.random() * 100)
   );
   const oneOrZero = Math.round(exitPercentage / 100) * 100;
- 
 
   // fetch notes
   useEffect(() => {
     async function fetchNotes() {
-      const api_service: SearchNotesApi = new TestSearchNotesApi();
-      const notes = await api_service.search('', 'all', 50, 0);
+      console.log('fetching notes');
+      const api_service: ISearchNotesApi = new SearchNotesApi();
+      const notes = await api_service.search(
+        RestNotesSearchType.LATEST,
+        'all',
+        50,
+        0
+      );
       console.log(notes);
-      setNotes(notes);
     }
     fetchNotes();
-  }, [])
+  }, []);
 
   const cards = useMemo(() => {
-    return notes.map((note) => (
-      <NoteCard key={note.id} note={note} ></NoteCard>
-    ))
-  }, [notes])
+    return notes.map((note) => <NoteCard key={note.id} note={note}></NoteCard>);
+  }, [notes]);
 
   const MainContent = () => (
-      <Box
+    <Box
       sx={{
         height: '100%',
         width: '100%',
         alignSelf: 'center',
-        fontFamily: "Open Sans",
+        fontFamily: 'Open Sans',
         display: 'flex',
-        overflow: 'auto'
+        overflow: 'auto',
       }}
     >
       <TopBar></TopBar>
       {/* add padding of the actual margin, topbar, and margin of top bar */}
-      <Box sx={{pt: `calc(${M4} + ${M5} + ${M3})`, height: 'calc(100% - 8rem)', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Box
+        sx={{
+          pt: `calc(${M4} + ${M5} + ${M3})`,
+          height: 'calc(100% - 8rem)',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
         <CreateNote></CreateNote>
-        <Grid container spacing={M3} p={M4} width={'100%'} size={{ xs: 2, sm: 4, md: 4 }}>
-          {cards.map((c) => (<Grid key={c.key}>{c}</Grid>))}
+        <Grid
+          container
+          spacing={M3}
+          p={M4}
+          width={'100%'}
+          size={{ xs: 2, sm: 4, md: 4 }}
+        >
+          {cards.map((c) => (
+            <Grid key={c.key}>{c}</Grid>
+          ))}
         </Grid>
-      
       </Box>
     </Box>
   );
 
-    return (
+  return (
     <ThemeProvider theme={theme}>
       {/* Loading Animation for Loading Page
       which is as long visible as "overlay" as
@@ -129,32 +165,61 @@ export const MainPage: React.FC = () => {
   );
 };
 
-
 export const CreateNote: React.FC = () => {
   const { theme } = useThemeStore();
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState(note_of_date_at_hour());
   const [content, setContent] = useState('');
+  const { updateNote } = useNotesStore();
+  const [snackbarState, setSnackbarState] = useState({
+    open: false,
+  });
 
+  // store when component lost focus
+  useEffect(() => {
+    if (!open) {
+      save_note();
+    }
+  }, [open]);
 
-  return <Stack width={1/3} spacing={'1rem'} sx={{backgroundColor: theme.palette.background.paper, padding: '1rem', borderRadius: '1rem'}}>
-    
-      <AnimatePresence mode='wait' initial={false}>
-     {!open && (
-
-        <TextField
-        variant="outlined"
-        defaultValue={'New Note'}
-          onClick={() => setOpen(true)}
-          slotProps={
-            {
+  // saves note to backend and updates store
+  async function save_note() {
+    const api = new NoteApi();
+    const note = await api.post(title, content);
+    if (note !== undefined) {
+      setSnackbarState({ open: true });
+      updateNote(note);
+    }
+  }
+  return (
+    <Stack
+      width={1 / 3}
+      spacing={'1rem'}
+      sx={{
+        backgroundColor: theme.palette.background.paper,
+        padding: '1rem',
+        borderRadius: '1rem',
+      }}
+    >
+      <AnimatePresence mode="wait" initial={false}>
+        {!open && (
+          <TextField
+            variant="outlined"
+            defaultValue={'New Note'}
+            onClick={() => setOpen(true)}
+            slotProps={{
               input: {
                 startAdornment: (
                   <InputAdornment position="start">
-                    <CreateIcon sx={{ fontSize: '1.5rem', color: theme.palette.primary.light }} />
+                    <CreateIcon
+                      sx={{
+                        fontSize: '1.5rem',
+                        color: theme.palette.primary.light,
+                      }}
+                    />
                   </InputAdornment>
-                )
-              }
+                ),
+              },
             }}
             sx={{
               '& .MuiOutlinedInput-root': {
@@ -168,60 +233,68 @@ export const CreateNote: React.FC = () => {
                 '&.Mui-focused fieldset': {
                   borderColor: theme.palette.primary.light,
                 },
-              },            
+              },
             }}
-        > 
-        </TextField>
-      )}
+          ></TextField>
+        )}
 
-      {open && (
-        <motion.div
+        {open && (
+          <motion.div
             key="expanded"
             initial={{ opacity: 0, y: 0 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
             transition={{ duration: 0.2 }}
           >
-        <Paper
-          elevation={3}
-          // tab index and onBlur to close when clicking outside
-          tabIndex={-1}
-          onBlur={(e) => {
-            if (!e.currentTarget.contains(e.relatedTarget)) {
-              setOpen(false);
-            }
-          }}
-          sx={{
-            p: 2,
-            borderRadius: "1rem",
-            display: "flex",
-            flexDirection: "column",
-            gap: 1.5,
-          }}
-        >
-          <TextField
-            placeholder="Title"
-            variant="standard"
-            fullWidth
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            color='primary'
-          />
+            <Paper
+              elevation={3}
+              // tab index and onBlur to close when clicking outside
+              tabIndex={-1}
+              onBlur={(e) => {
+                if (!e.currentTarget.contains(e.relatedTarget)) {
+                  setOpen(false);
+                }
+              }}
+              sx={{
+                p: 2,
+                borderRadius: '1rem',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 1.5,
+              }}
+            >
+              <TextField
+                placeholder={title}
+                variant="standard"
+                fullWidth
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                color="primary"
+              />
 
-          <TextField
-            autoFocus
-            placeholder="Take a note..."
-            variant="outlined"
-            minRows={3}
-            multiline
-            fullWidth
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            color='secondary'
-          />
-        </Paper>
-        </motion.div>
-      )}
+              <TextField
+                autoFocus
+                placeholder="Take a note..."
+                variant="outlined"
+                minRows={3}
+                multiline
+                fullWidth
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                color="secondary"
+              />
+            </Paper>
+          </motion.div>
+        )}
       </AnimatePresence>
-  </Stack>;
-}
+      <Snackbar
+        open={snackbarState.open}
+        onClose={() => setSnackbarState({ open: false })}
+        slots={{ transition: Slide }}
+        message="Uploaded Note"
+        key={Slide.name}
+        autoHideDuration={1200}
+      />
+    </Stack>
+  );
+};
