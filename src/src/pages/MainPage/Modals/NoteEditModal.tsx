32429f5f2d3a0931c241111fcd45b3e2ac066795
kurama <paul.zenker.pz@gmail.com>
2025-12-26 22:45:28 +0100

    feat: small test with TipTap

--- src/src/pages/MainPage/Modals/NoteEditModal.tsx
+++ src/src/pages/MainPage/Modals/NoteEditModal.tsx
@@ -1,10 +1,39 @@
-import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+} from '@mui/material';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
 import CloseIcon from '@mui/icons-material/Close';
-import { useEffect } from 'react';
-
+import { useEffect, useRef } from 'react';
+import { useThemeStore } from '../../../zustand/useThemeStore';
+import { M1, M2, M3, M4 } from '../../../statics';
+import {
+  MenuButtonAddTable,
+  MenuButtonBold,
+  MenuButtonBulletedList,
+  MenuButtonCode,
+  MenuButtonCodeBlock,
+  MenuButtonItalic,
+  MenuButtonOrderedList,
+  MenuButtonTaskList,
+  MenuControlsContainer,
+  MenuDivider,
+  MenuSelectHeading,
+  RichTextEditor,
+  TableBubbleMenu,
+  type RichTextEditorRef,
+} from 'mui-tiptap';
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from '@tiptap/extension-table';
 interface NoteEditorModalProps {
   open: boolean;
   onClose: () => void;
@@ -20,32 +49,24 @@ export const NoteEditorModal: React.FC<NoteEditorModalProps> = ({
   content,
   onSave,
 }) => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit,
-      Placeholder.configure({
-        placeholder: 'Start typing...',
-      }),
-    ],
-    content,
-    onUpdate: ({ editor }) => {
-      onSave(editor.getHTML());
-    },
-  });
-
-  useEffect(() => {
-    if (editor && content !== editor.getHTML()) {
-      editor.commands.setContent(content);
-    }
-  }, [content, editor]);
+  const { theme } = useThemeStore();
+  const rteRef = useRef<RichTextEditorRef>(null);
 
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="md"
+      sx={{ color: theme.palette.text.primary }}
+      fullWidth
+    >
       <DialogTitle
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
+          backgroundColor: theme.palette.background.paper,
+          padding: M2,
         }}
       >
         {title}
@@ -53,16 +74,47 @@ export const NoteEditorModal: React.FC<NoteEditorModalProps> = ({
           <CloseIcon />
         </IconButton>
       </DialogTitle>
-      <DialogContent>
-        <EditorContent
-          editor={editor}
-          style={{
-            minHeight: '300px',
-            padding: '16px',
-            border: '1px solid #ddd',
-            borderRadius: '4px',
-          }}
-        />
+      <DialogContent
+        sx={{
+          backgroundColor: theme.palette.background.default,
+          padding: M3,
+          minHeight: '40vh',
+          display: 'flex',
+        }}
+      >
+        <RichTextEditor
+          ref={rteRef}
+          sx={{ height: '100%', width: '100%', mt: M3 }}
+          extensions={[
+            StarterKit,
+            Table.configure({
+              resizable: true,
+            }),
+            TableRow,
+            TableCell,
+            TableHeader,
+          ]}
+          content={content}
+          renderControls={() => (
+            <MenuControlsContainer>
+              <MenuSelectHeading />
+              <MenuDivider />
+              <MenuButtonBold />
+              <MenuButtonItalic />
+              <MenuButtonCodeBlock />
+              <MenuButtonBulletedList />
+              <MenuButtonOrderedList />
+              {/* <MenuButtonTaskList /> */}
+              <MenuButtonAddTable />
+            </MenuControlsContainer>
+          )}
+        >
+          {() => (
+            <>
+              <TableBubbleMenu />
+            </>
+          )}
+        </RichTextEditor>
       </DialogContent>
     </Dialog>
   );
