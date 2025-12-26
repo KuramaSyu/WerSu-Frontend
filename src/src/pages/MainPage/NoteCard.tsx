3e2da140cbe8f3cac60578dd93e0f2b84c3795d7
kurama <paul.zenker.pz@gmail.com>
2025-12-26 09:45:19 +0100

    chore: trim note content

--- src/src/pages/MainPage/NoteCard.tsx
+++ src/src/pages/MainPage/NoteCard.tsx
@@ -1,24 +1,32 @@
-import { Card, CardContent, Typography } from "@mui/material";
-import type { MinimalNote } from "../../api/SearchNotesApi";
-import { M2, M3, M4 } from "../../statics";
-import { useThemeStore } from "../../zustand/useThemeStore";
-import { blendWithContrast } from "../../utils/blendWithContrast";
+import { Card, CardContent, Typography } from '@mui/material';
+import { M2, M3, M4 } from '../../statics';
+import { useThemeStore } from '../../zustand/useThemeStore';
+import { blendWithContrast } from '../../utils/blendWithContrast';
+import type { MinimalNote } from '../../api/models/search';
 
-export const NoteCard: React.FC<{note: MinimalNote}> = ({note}) => {
-    const {theme} = useThemeStore();
-    return (
-        <Card sx={{ minWidth: '4rem' }} variant="outlined">
-            <CardContent>
-                <Typography variant="subtitle2" mb={M3} color={blendWithContrast(theme.palette.text.primary, theme, 1/4)}>
-                    {note.updated_at}
-                </Typography>
-                <Typography variant="h5" gutterBottom>
-                    {note.title}
-                </Typography>
-                <Typography variant="body2" color={theme.palette.text.secondary} sx={{ whiteSpace: 'pre-wrap' }}>
-                    {note.stripped_content}
-                </Typography>
-            </CardContent>
-        </Card>
-    );
+export const NoteCard: React.FC<{ note: MinimalNote }> = ({ note }) => {
+  const { theme } = useThemeStore();
+  return (
+    <Card sx={{ minWidth: '4rem' }} variant="outlined">
+      <CardContent>
+        <Typography
+          variant="subtitle2"
+          mb={M3}
+          color={blendWithContrast(theme.palette.text.primary, theme, 1 / 4)}
+        >
+          {note.updated_at}
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          {note.title}
+        </Typography>
+        <Typography
+          variant="body2"
+          color={theme.palette.text.secondary}
+          sx={{ whiteSpace: 'pre-wrap' }}
+        >
+          {note.stripped_content.substring(0, 100)}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
