892a6f25525a618a48d8d605e1fecbb932938f63
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-03-25 16:56:44 +0100

    chore: way to disable animations

--- src/src/pages/MainPage/Main.tsx
+++ src/src/pages/MainPage/Main.tsx
@@ -9,33 +9,34 @@ import {
   Stack,
   TextField,
   ThemeProvider,
-} from '@mui/material';
-import { useThemeStore } from '../../zustand/useThemeStore';
-import { AnimatePresence, motion } from 'framer-motion';
+  Typography,
+} from "@mui/material";
+import { useThemeStore } from "../../zustand/useThemeStore";
+import { AnimatePresence, motion } from "framer-motion";
 
-import { Logo } from '../../components/logo';
-import TopBar from '../../components/TopBar';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import CreateIcon from '@mui/icons-material/Create';
+import { Logo } from "../../components/logo";
+import TopBar from "../../components/TopBar";
+import { useEffect, useMemo, useRef, useState } from "react";
+import CreateIcon from "@mui/icons-material/Create";
 import {
   SearchNotesApi,
   TestSearchNotesApi,
   type ISearchNotesApi,
-} from '../../api/SearchNotesApi';
-import { useSearchNotesStore } from '../../zustand/useSearchNotesStore';
-import { NoteCard } from './NoteCard';
-import { M1, M2, M3, M4, M5, M6 } from '../../statics';
-import { useUserStore } from '../../zustand/userStore';
-import { useLoadingStore } from '../../zustand/loadingStore';
-import { LoadingPage } from '../LoadingPage/Main';
-import { useBreakpoint } from '../../hooks/useBreakpoint';
-import { LoginPage } from '../LoginPage/Main';
-import { RestNotesSearchType } from '../../api/models/search';
-import { note_of_date_at_hour } from '../../utils/NoteTitleTemplates';
-import { NoteApi } from '../../api/NoteApi';
-import { useNotesStore } from '../../zustand/useNotesStore';
-import { CardGrid } from './CardGrid';
-import { MainContent } from './MainContent';
+} from "../../api/SearchNotesApi";
+import { useSearchNotesStore } from "../../zustand/useSearchNotesStore";
+import { NoteCard } from "./NoteCard";
+import { M1, M2, M3, M4, M5, M6 } from "../../statics";
+import { useUserStore } from "../../zustand/userStore";
+import { useLoadingStore } from "../../zustand/loadingStore";
+import { LoadingPage } from "../LoadingPage/Main";
+import { useBreakpoint } from "../../hooks/useBreakpoint";
+import { LoginPage } from "../LoginPage/Main";
+import { RestNotesSearchType } from "../../api/models/search";
+import { note_of_date_at_hour } from "../../utils/NoteTitleTemplates";
+import { NoteApi } from "../../api/NoteApi";
+import { useNotesStore } from "../../zustand/useNotesStore";
+import { CardGrid } from "./CardGrid";
+import { MainContent } from "./MainContent";
 
 export const MainPage: React.FC = () => {
   const { theme } = useThemeStore();
@@ -43,51 +44,61 @@ export const MainPage: React.FC = () => {
   const { isLoading } = useLoadingStore();
   const { isMobile } = useBreakpoint();
   const [exitPercentage, setExitPercentage] = useState(
-    Math.round(Math.random() * 100)
+    Math.round(Math.random() * 100),
   );
   const oneOrZero = Math.round(exitPercentage / 100) * 100;
+  const DISABLE_LOADING_ANIMATION = false; // Set to true to disable the loading animation
 
   return (
     <ThemeProvider theme={theme}>
       {/* Loading Animation for Loading Page
       which is as long visible as "overlay" as
       it needs to fetch all resources via REST. */}
-      <AnimatePresence initial={false}>
-        {isLoading && (
-          <motion.div
-            initial={false}
-            animate={{ clipPath: 'circle(100% at 50% 50%)' }}
-            exit={{
-              clipPath: oneOrZero
-                ? `circle(0% at 100% ${exitPercentage}%)`
-                : `circle(0% at ${exitPercentage}% 100%)`,
-              opacity: 0.2,
-            }}
-            transition={{
-              duration: 1,
-              ease: [0.4, 0, 0.2, 1],
-            }}
-            style={{
-              position: 'fixed',
-              zIndex: 9999,
-              top: 0,
-              left: 0,
-              width: '100vw',
-              height: '100vh',
-            }}
-          >
-            <LoadingPage></LoadingPage>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      {!DISABLE_LOADING_ANIMATION && (
+        <AnimatePresence initial={false}>
+          {isLoading && (
+            <motion.div
+              initial={false}
+              animate={{ clipPath: "circle(100% at 50% 50%)" }}
+              exit={{
+                clipPath: oneOrZero
+                  ? `circle(0% at 100% ${exitPercentage}%)`
+                  : `circle(0% at ${exitPercentage}% 100%)`,
+                opacity: 0.2,
+              }}
+              transition={{
+                duration: 1,
+                ease: [0.4, 0, 0.2, 1],
+              }}
+              style={{
+                position: "fixed",
+                zIndex: 9999,
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+              }}
+            >
+              <LoadingPage></LoadingPage>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      )}
+      {isLoading && DISABLE_LOADING_ANIMATION && (
+        <>
+          <Box zIndex={-1} top={0} left={0} position="fixed">
+            <LoadingPage />
+          </Box>
+        </>
+      )}
 
       {/* Box for either the Main App or Login Page, depending on user state */}
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          height: '100%',
-          overflow: 'hidden', // Prevents overflow
+          display: "flex",
+          flexDirection: "row",
+          height: "100%",
+          overflow: "hidden", // Prevents overflow
           paddingTop: user !== null && !isMobile ? M1 : undefined,
         }}
       >
@@ -95,7 +106,7 @@ export const MainPage: React.FC = () => {
           <MainContent></MainContent>
         ) : (
           <>
-            {console.log('render login page')}
+            {console.log("render login page")}
             <LoginPage></LoginPage>
           </>
         )}
