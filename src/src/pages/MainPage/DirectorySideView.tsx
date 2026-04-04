02913cf2fdd9c00e4e7c3897759e0c96dd6bb005
kurama <paul.zenker.pz@gmail.com>
2026-04-04 19:25:49 +0200

    feat: make directory items droppable

--- src/src/pages/MainPage/DirectorySideView.tsx
+++ src/src/pages/MainPage/DirectorySideView.tsx
@@ -1,60 +1,75 @@
-import { Card, Stack } from "@mui/material";
-import { useDirectoryStore } from "../../zustand/useDirectoryStore";
 import * as React from "react";
+import { useDroppable } from "@dnd-kit/react";
 import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
+import { useDirectoryStore } from "../../zustand/useDirectoryStore";
 import {
   DirectoryHierarchyBuilder,
-  NoteHierarchyBuilder,
-  RootHirarchyItem,
   type HirarchyItem,
 } from "../../models/HirarchyItem";
-import { useNotesStore } from "../../zustand/useNotesStore";
+
+interface DirectoryTreeNodeProps {
+  item: HirarchyItem;
+}
+
+/**
+ * Recursive tree node that registers each directory item as a dnd-kit
+ * droppable target.
+ */
+const DirectoryTreeNode: React.FC<DirectoryTreeNodeProps> = ({ item }) => {
+  const itemId = item.getId();
+  const { ref, isDropTarget } = useDroppable({
+    id: itemId,
+    type: "directory",
+    accept: "note",
+    data: {
+      directoryId: itemId,
+      directoryName: item.getName(),
+    },
+  });
+  const bg = null;
+
+  return (
+    <TreeItem
+      key={itemId}
+      itemId={itemId}
+      ref={ref}
+      label={item.getName()}
+      sx={{
+        "& > .MuiTreeItem-content": {
+          minHeight: 50,
+          borderRadius: 1,
+          outline: isDropTarget ? "2px dashed rgba(255,255,255,0.7)" : "none",
+          outlineOffset: isDropTarget ? "-2px" : "0px",
+          backgroundImage: bg
+            ? "linear-gradient(rgba(0,0,0,.35), rgba(0,0,0,.35)), url(" +
+              bg +
+              ")"
+            : undefined,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          color: bg ? "#fff" : "inherit",
+        },
+        "& > .MuiTreeItem-content.Mui-selected": {
+          backgroundColor: "rgba(255,255,255,0.18)",
+        },
+      }}
+    >
+      {item.getChildren().map((child) => (
+        <DirectoryTreeNode key={child.getId()} item={child} />
+      ))}
+    </TreeItem>
+  );
+};
 
 export const DirectorySideView: React.FC = () => {
   const { directoriesById } = useDirectoryStore();
-  const {} = useNotesStore();
-  const bg = null;
-
   const directoryHirarchy = new DirectoryHierarchyBuilder(
     directoriesById,
   ).build("Stacks");
 
-  /**
-   * recursively generates tree items for a given hierarchy item and its children
-   * @param item the root item to construct subitems for
-   */
-  const generateTreeItems = (item: HirarchyItem): React.ReactNode => {
-    return (
-      <TreeItem
-        key={item.getId()}
-        itemId={item.getId()}
-        label={item.getName()}
-        sx={{
-          "& > .MuiTreeItem-content": {
-            minHeight: 50,
-            borderRadius: 1,
-            backgroundImage: bg
-              ? "linear-gradient(rgba(0,0,0,.35), rgba(0,0,0,.35)), url(" +
-                bg +
-                ")"
-              : undefined,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            color: bg ? "#fff" : "inherit",
-          },
-          "& > .MuiTreeItem-content.Mui-selected": {
-            backgroundColor: "rgba(255,255,255,0.18)",
-          },
-        }}
-      >
-        {item.getChildren().map((child) => generateTreeItems(child))}
-      </TreeItem>
-    );
-  };
-
   return (
     <SimpleTreeView defaultExpandedItems={[directoryHirarchy.getId()]}>
-      {generateTreeItems(directoryHirarchy)}
+      <DirectoryTreeNode item={directoryHirarchy} />
     </SimpleTreeView>
   );
 };
