9bc7d819940fb498d5dd5746f5234f0c646bcf33
kurama <paul.zenker.pz@gmail.com>
2026-04-04 18:45:29 +0200

    ui: use a tree view for directories

--- src/src/pages/MainPage/DirectorySideView.tsx
+++ src/src/pages/MainPage/DirectorySideView.tsx
@@ -1,15 +1,60 @@
 import { Card, Stack } from "@mui/material";
 import { useDirectoryStore } from "../../zustand/useDirectoryStore";
+import * as React from "react";
+import { SimpleTreeView, TreeItem } from "@mui/x-tree-view";
+import {
+  DirectoryHierarchyBuilder,
+  NoteHierarchyBuilder,
+  RootHirarchyItem,
+  type HirarchyItem,
+} from "../../models/HirarchyItem";
+import { useNotesStore } from "../../zustand/useNotesStore";
 
-export const DirectorySideView = () => {
+export const DirectorySideView: React.FC = () => {
   const { directoriesById } = useDirectoryStore();
+  const {} = useNotesStore();
+  const bg = null;
+
+  const directoryHirarchy = new DirectoryHierarchyBuilder(
+    directoriesById,
+  ).build("Stacks");
+
+  /**
+   * recursively generates tree items for a given hierarchy item and its children
+   * @param item the root item to construct subitems for
+   */
+  const generateTreeItems = (item: HirarchyItem): React.ReactNode => {
+    return (
+      <TreeItem
+        key={item.getId()}
+        itemId={item.getId()}
+        label={item.getName()}
+        sx={{
+          "& > .MuiTreeItem-content": {
+            minHeight: 50,
+            borderRadius: 1,
+            backgroundImage: bg
+              ? "linear-gradient(rgba(0,0,0,.35), rgba(0,0,0,.35)), url(" +
+                bg +
+                ")"
+              : undefined,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            color: bg ? "#fff" : "inherit",
+          },
+          "& > .MuiTreeItem-content.Mui-selected": {
+            backgroundColor: "rgba(255,255,255,0.18)",
+          },
+        }}
+      >
+        {item.getChildren().map((child) => generateTreeItems(child))}
+      </TreeItem>
+    );
+  };
+
   return (
-    <Stack direction={"column"}>
-      {Object.values(directoriesById).map((directory) => (
-        <Card key={directory.id} variant="outlined" sx={{ mb: 1, p: 1 }}>
-          {directory.display_name || directory.name}
-        </Card>
-      ))}
-    </Stack>
+    <SimpleTreeView defaultExpandedItems={[directoryHirarchy.getId()]}>
+      {generateTreeItems(directoryHirarchy)}
+    </SimpleTreeView>
   );
 };
