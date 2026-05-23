bbe3cc106462ed0793bb33734f1350620e0d4a13
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-05-23 23:38:06 +0200

    feat,ux: speeddial to create new note

---
+++ src/src/components/NewNoteSpeedDial.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
+import CreateIcon from "@mui/icons-material/Create";
+import { CreateNote } from "../pages/MainPage/CreateNote";
+
+const NewNoteSpeedDial: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  return (
+    <>
+      <SpeedDial
+        ariaLabel="New note actions"
+        icon={<SpeedDialIcon />}
+        direction="up"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        sx={{
+          position: "fixed",
+          bottom: 16,
+          right: 16,
+          zIndex: (theme) => theme.zIndex.appBar + 2,
+        }}
+      >
+        <SpeedDialAction
+          icon={<CreateIcon />}
+          tooltipTitle="New note"
+          onClick={() => {
+            setDialogOpen(true);
+            setOpen(false);
+          }}
+        />
+      </SpeedDial>
+
+      <CreateNote open={dialogOpen} onOpenChange={setDialogOpen} />
+    </>
+  );
+};
+
+export default NewNoteSpeedDial;
