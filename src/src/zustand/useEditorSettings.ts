1877751e4c4617c4a9742fe5ab8efd9bf085856b
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-10 18:20:20 +0200

    api: added setViewMode to EditorSettings zustand

--- src/src/zustand/useEditorSettings.ts
+++ src/src/zustand/useEditorSettings.ts
@@ -4,10 +4,12 @@ interface EditorSettingsState {
   write: boolean;
   setWrite: (write_enabled: boolean) => void;
   viewMode: "source" | "rich";
+  setViewMode: (mode: "source" | "rich") => void;
 }
 
 export const useEditorSettings = create<EditorSettingsState>((set) => ({
   write: false,
   setWrite: (write_enabled: boolean) => set({ write: write_enabled }),
   viewMode: "rich",
+  setViewMode: (mode: "source" | "rich") => set({ viewMode: mode }),
 }));
