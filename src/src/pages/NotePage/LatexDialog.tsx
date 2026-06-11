a78986398911d45fe9e6eb7658f19e5bd957bfba
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-12 01:45:34 +0200

    feat: added tooltips to button groups

--- src/src/pages/NotePage/LatexDialog.tsx
+++ src/src/pages/NotePage/LatexDialog.tsx
@@ -1,11 +1,16 @@
 import {
   Box,
+  Button,
+  ButtonGroup,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
   IconButton,
   Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
 } from "@mui/material";
 
 import { useEffect, useState } from "react";
@@ -13,38 +18,74 @@ import CodeMirror from "@uiw/react-codemirror";
 import { latex } from "codemirror-lang-latex";
 import DoneIcon from "@mui/icons-material/Done";
 import CloseIcon from "@mui/icons-material/Close";
-import { BlockMath } from "react-katex";
+import { BlockMath, InlineMath } from "react-katex";
 
 export interface LatexDialogProps {
+  // compressed is assumed, when latex start with \displaystyle
   open: boolean;
-  onClose: (latex: string) => void;
-  onCancel?: (latex: string) => void;
+  onClose: (
+    latex: string,
+    latexType: "inline" | "block",
+    compressed: boolean,
+  ) => void;
+  setOpen?: (open: boolean) => void;
+  onCancel?: (
+    latex: string,
+    latexType: "inline" | "block",
+    compressed: boolean,
+  ) => void;
   latexCode?: string;
+  initialLatexType?: "inline" | "block";
 }
+
 export const LatexDialog: React.FC<LatexDialogProps> = ({
   open,
   onClose,
   onCancel,
   latexCode,
+  initialLatexType,
+  setOpen,
 }) => {
   const [value, setValue] = useState(latexCode || "");
-  const [latexType, setLatexType] = useState<"inline" | "block">("inline");
+  const [latexType, setLatexType] = useState<"inline" | "block">(
+    initialLatexType || "inline",
+  );
+
+  // compressed latex means its good for one line
+  const [compressed, setCompressed] = useState(false);
   const onCloseWrap = (result: string) => {
-    onClose(result);
+    // readd the removed displaystyle, which was converted into the compressed state
+    if (!compressed) {
+      result = `\\displaystyle ${result}`;
+    }
+
+    onClose(result, latexType, compressed);
+    setOpen?.(false);
     setValue("");
   };
 
   // on re-opens, we need to insert LatexCode (passed as prop) into the editor
   useEffect(() => {
-    setValue(latexCode ?? "");
+    if (latexCode?.startsWith("\\displaystyle")) {
+      // this means, integrals, fractions, sums etc. are written big, not one line
+      setCompressed(false);
+      // replace the first displaystyle with an empty string
+      setValue(latexCode.replace(/\\displaystyle\s*/, ""));
+    } else {
+      setValue(latexCode || "");
+      setCompressed(true);
+    }
+    setLatexType(initialLatexType || "inline");
   }, [open]);
 
+  //
   return (
     <Dialog
       open={open}
       onClose={(_, reason) => {
         if (reason === "backdropClick" || reason === "escapeKeyDown") {
-          onCancel?.(value);
+          onCancel?.(value, latexType, compressed);
+          setOpen?.(false);
           return;
         }
         onCloseWrap("");
@@ -93,17 +134,70 @@ export const LatexDialog: React.FC<LatexDialogProps> = ({
               width: "100%",
             }}
           >
-            <BlockMath math={value} />
+            {compressed ? (
+              <InlineMath math={value} />
+            ) : (
+              <BlockMath math={value} />
+            )}
           </Box>
         </Stack>
       </DialogContent>
-      <DialogActions>
-        <IconButton title="ok" onClick={() => onCloseWrap(value)}>
-          <DoneIcon />
-        </IconButton>
-        <IconButton title="cancel" onClick={() => onCancel?.(value)}>
-          <CloseIcon />
-        </IconButton>
+      <DialogActions sx={{ justifyContent: "space-between" }}>
+        <ToggleButtonGroup
+          value={latexType}
+          onChange={(_, newType) => setLatexType(newType)}
+          exclusive
+          size="small"
+        >
+          <Tooltip title="Math is within a textline">
+            <ToggleButton value="inline">Inline</ToggleButton>
+          </Tooltip>
+          <Tooltip title="Math has it's own line">
+            <ToggleButton value="block">Block</ToggleButton>
+          </Tooltip>
+        </ToggleButtonGroup>
+
+        <ToggleButtonGroup
+          value={compressed ? "compressed" : "notCompressed"}
+          onChange={(_, newValue) => setCompressed(newValue === "compressed")}
+          exclusive
+          size="small"
+        >
+          <Tooltip
+            title={
+              <>
+                smaller symbols: <InlineMath math={"\\int_a^b f(x) dx"} />{" "}
+              </>
+            }
+          >
+            <ToggleButton value="compressed">small Notation</ToggleButton>
+          </Tooltip>
+          <Tooltip
+            title={
+              <>
+                bigger symbols:{" "}
+                <InlineMath math={"\\displaystyle \\int_a^b f(x) dx"} />{" "}
+              </>
+            }
+          >
+            <ToggleButton value="notCompressed">big Notation</ToggleButton>
+          </Tooltip>
+        </ToggleButtonGroup>
+
+        <Box>
+          <IconButton title="ok" onClick={() => onCloseWrap(value)}>
+            <DoneIcon />
+          </IconButton>
+          <IconButton
+            title="cancel"
+            onClick={() => {
+              onCancel?.(value, latexType, compressed);
+              setOpen?.(false);
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </Box>
       </DialogActions>
     </Dialog>
   );
