5a3b589b4115823ad1ad277af86dd01b3aac04f6
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-16 23:29:20 +0200

    api: added endpoint for fetching accesstokens

--- src/src/api/UserApi.tsx
+++ src/src/api/UserApi.tsx
@@ -3,10 +3,13 @@ import type { DiscordUser } from "../components/DiscordLogin";
 
 import { DiscordUserImpl } from "../components/DiscordLogin";
 import { useUsersStore, useUserStore } from "../zustand/userStore";
+import type { GetAcccessTokenResponse } from "./models/auth";
+import { th } from "zod/v4/locales";
 
 export interface BackendApiInterface {}
 export interface UserApiInterface {
   fetchUser(): Promise<Response>;
+  fetchAccessToken(): Promise<GetAcccessTokenResponse>;
 }
 
 // Class, to fetch resources from the backend. Responses will be
@@ -45,5 +48,27 @@ export class UserApi implements UserApiInterface {
       this.logError(`/api/auth/user`, response.json());
     }
     return response;
+  }
+
+  /**
+   * fetches an access token from the backend
+   * It sets `useUserStore` to the fetched token
+   */
+  async fetchAccessToken(): Promise<GetAcccessTokenResponse> {
+    const url = `${BACKEND_BASE}/api/auth/access-token`;
+    const response = await fetch(url, {
+      credentials: "include",
+    });
+    if (response.ok) {
+      const tokenData: { token: string } = await response.json().catch((e) => {
+        this.logError(url, e);
+        throw e;
+      });
+      if (tokenData.token) {
+        return tokenData;
+      }
+    }
+    this.logError(url, response.json());
+    throw new Error("Failed to fetch access token");
   }
 }
