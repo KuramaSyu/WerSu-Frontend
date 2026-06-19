5caad026c15106161f0ad6c4eb980cbf1b070794
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-06-19 14:02:05 +0200

    api: added useUsers for later use

--- src/src/api/queries/useUser.ts
+++ src/src/api/queries/useUser.ts
@@ -1,7 +1,10 @@
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import { UserApi } from "../UserApi";
 import { useAuthStore } from "../../zustand/useAuthStore";
-import { DiscordUserImpl } from "../../components/DiscordLogin";
+import {
+  DiscordUserImpl,
+  type DiscordUser,
+} from "../../components/DiscordLogin";
 
 /**
  * Hook to fetch the current user with discord login authentication
@@ -11,11 +14,37 @@ export function useUser(): UseQueryResult<DiscordUserImpl, Error> {
   return useQuery({
     queryKey: ["user"],
     queryFn: async () => {
-      const data = await new UserApi().fetchUser();
-      return new DiscordUserImpl(data);
+      return await new UserApi().fetchUser();
     },
 
     // the cached entry is plain JSON -> recreate class
     select: (data) => new DiscordUserImpl(data),
+  });
+}
+
+/**
+ * Hook to fetch all users the current user has access to, including their friends
+ * @returns
+ */
+export function useUsers(
+  userIds: string[],
+): UseQueryResult<Record<string, DiscordUser>, Error> {
+  return useQuery({
+    queryKey: ["users", userIds],
+    queryFn: async () => {
+      if (userIds.length === 0) {
+        return [];
+      }
+      return await new UserApi().fetchUsers(userIds);
+    },
+
+    // the cached entry is plain JSON -> recreate class
+    select: (data) => {
+      var users: Record<string, DiscordUser> = {};
+      for (const user of data) {
+        users[user.id] = new DiscordUserImpl(user);
+      }
+      return users;
+    },
   });
 }
