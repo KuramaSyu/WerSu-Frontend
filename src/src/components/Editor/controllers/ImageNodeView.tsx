8692becd125a5c98fcf05a1823110f6ed4440787
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-07-03 21:09:18 +0200

    feat: load iamges for public users with temp JWT

--- src/src/components/Editor/controllers/ImageNodeView.tsx
+++ src/src/components/Editor/controllers/ImageNodeView.tsx
@@ -1,25 +1,60 @@
 import { Box } from "@mui/material";
-import { NodeViewWrapper, type NodeViewProps } from "@tiptap/react";
+import {
+  NodeViewWrapper,
+  useCurrentEditor,
+  type NodeViewProps,
+} from "@tiptap/react";
 import { useThemeStore } from "../../../zustand/useThemeStore";
-import { useEffect } from "react";
-import { M1, M2 } from "../../../statics";
+import { useMemo, useRef } from "react";
+import { M2 } from "../../../statics";
 import { useEditorSettings } from "../../../zustand/useEditorSettings";
+import { useAuthStore } from "../../../zustand/useAuthStore";
+import { AttachmentApi } from "../../../api/AttachmentApi";
+import { AttachmentLinkBuilder } from "../../../api/utils/AttachmentLInkBuilder";
+import { extractAttachmentKeyFromUrl } from "../../../api/utils/request_helpers";
+import { useUser } from "../../../api/queries/useUser";
 
 export function ImageNodeView({ node, selected, getPos }: NodeViewProps) {
   const { theme } = useThemeStore();
   const { editMode } = useEditorSettings();
+
+  // use attachment tokens, so that public users can access images
+  const { shareAttachmentTokens } = useAuthStore();
+
+  // To grant image access to public users,
+  // we need to patch the URL by appending an JWT
+  // just generated for public users for this one attachment 15 minutes.
+  const resolvedSrc = (src: string) => {
+    const attachmentKey = extractAttachmentKeyFromUrl(src);
+
+    if (!attachmentKey) return src;
+    const jwt = shareAttachmentTokens[attachmentKey];
+    if (!jwt) {
+      console.warn(
+        `No share-attachment JWT found for attachment key ${attachmentKey}. The image WILL not load for public users.`,
+      );
+      return src;
+    }
+    return new AttachmentLinkBuilder(new AttachmentApi())
+      .setJwt(jwt)
+      .getLink(attachmentKey);
+  };
+
+  const src: string = resolvedSrc(node.attrs.src ?? "");
+
+  console.log(
+    "ImageNodeView - rerender; authstore has token?",
+    Object.keys(shareAttachmentTokens).length > 0,
+    "has exact token: ",
+    !!shareAttachmentTokens[extractAttachmentKeyFromUrl(src)],
+  );
 
   return (
     <NodeViewWrapper
       data-drag-handle
       contentEditable={false}
       className={selected ? "selected-image" : ""}
-      style={
-        {
-          // display: "inline-block",
-          // width fit-content:
-        }
-      }
+      //style={}
     >
       <Box
         sx={{
@@ -28,9 +63,9 @@ export function ImageNodeView({ node, selected, getPos }: NodeViewProps) {
         }}
       >
         <Box
-          component="img"
-          src={node.attrs.src}
-          alt={node.attrs.alt}
+          component={"img"}
+          src={src}
+          alt={src}
           sx={{
             display: "block",
 
