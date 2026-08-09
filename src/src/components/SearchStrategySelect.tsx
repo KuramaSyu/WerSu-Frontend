eb49514eeb03a1bda3216ab16281d5059b3830af
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-08-09 21:17:42 +0200

    ui: swtiched to CallapseToggleButton

--- src/src/components/SearchStrategySelect.tsx
+++ src/src/components/SearchStrategySelect.tsx
@@ -1,10 +1,18 @@
 import React, { memo } from "react";
-import { ToggleButtonGroup, ToggleButton, Tooltip } from "@mui/material";
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Tooltip,
+  Collapse,
+  Typography,
+  Box,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
 import { M4 } from "../statics";
 import { RestNotesSearchType } from "../api/models/search";
+import CollapseToggleButton from "./CollapseToggleButton";
 
 interface Props {
   searchType: RestNotesSearchType;
@@ -31,11 +39,15 @@ const SearchStrategySelect: React.FC<Props> = ({
       sx={{
         borderRadius: M4,
         width: "100%",
+        py: 0,
+        my: 0,
       }}
       // color="inherit"
     >
       <Tooltip title="Keyword: Search for exact matches">
-        <ToggleButton
+        <CollapseToggleButton
+          whenSelected={<Box sx={{ whiteSpace: "nowrap" }}>Keyword</Box>}
+          selected={searchType === RestNotesSearchType.KEYWORD}
           color={color}
           value={RestNotesSearchType.KEYWORD}
           aria-label="keyword"
@@ -43,36 +55,38 @@ const SearchStrategySelect: React.FC<Props> = ({
             borderTopLeftRadius: M4,
             borderBottomLeftRadius: M4,
             gap: 1,
-            width: "33.3%",
           }}
         >
           <SearchIcon />
-        </ToggleButton>
+        </CollapseToggleButton>
       </Tooltip>
       <Tooltip title="Typo Tolerant: Search with typo tolerance">
-        <ToggleButton
+        <CollapseToggleButton
+          whenSelected={<Box>Fuzzy</Box>}
+          selected={searchType === RestNotesSearchType.TYPO_TOLERANT}
           color={color}
           value={RestNotesSearchType.TYPO_TOLERANT}
           aria-label="typo tolerant"
-          sx={{ gap: 1, width: "33.3%" }}
+          sx={{ gap: 1 }}
         >
           <ManageSearchIcon />
-        </ToggleButton>
+        </CollapseToggleButton>
       </Tooltip>
       <Tooltip title="Context: Search for notes with similar content">
-        <ToggleButton
+        <CollapseToggleButton
           color={color}
+          whenSelected={<Box>Context</Box>}
+          selected={searchType === RestNotesSearchType.CONTEXT}
           value={RestNotesSearchType.CONTEXT}
           aria-label="context"
           sx={{
             borderTopRightRadius: M4,
             borderBottomRightRadius: M4,
             gap: 1,
-            width: "33.3%",
           }}
         >
           <AutoAwesomeIcon />
-        </ToggleButton>
+        </CollapseToggleButton>
       </Tooltip>
     </ToggleButtonGroup>
   );
