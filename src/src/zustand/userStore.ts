cc912d4b30c79df04f9f90777387f75e9b31180e
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-08-09 23:49:28 +0200

    chore: adopt user class rename

--- src/src/zustand/userStore.ts
+++ src/src/zustand/userStore.ts
@@ -1,18 +1,18 @@
-import { create } from 'zustand';
-import { DiscordUserImpl } from './../components/DiscordLogin';
+import { create } from "zustand";
+import { WersuUserImpl } from "./../components/DiscordLogin";
 
 interface UserState {
-  user: DiscordUserImpl | null;
-  setUser: (user: DiscordUserImpl | null) => void;
+  user: WersuUserImpl | null;
+  setUser: (user: WersuUserImpl | null) => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
-  setUser: (user: DiscordUserImpl | null) => set({ user: user }),
+  setUser: (user: WersuUserImpl | null) => set({ user: user }),
 }));
 
 export interface UsersState {
-  users: Record<string, DiscordUserImpl>;
+  users: Record<string, WersuUserImpl>;
   friendsLoaded: boolean;
 
   /**
@@ -20,14 +20,14 @@ export interface UsersState {
    * @param user - the user to add
    * @returns
    */
-  addUser: (user: DiscordUserImpl) => void;
+  addUser: (user: WersuUserImpl) => void;
 
   /**
    * extends a dict of users where existing users will be overridden with new ones
    * @param users - the users to add
    * @returns
    */
-  addFriends: (users: DiscordUserImpl[]) => void;
+  addFriends: (users: WersuUserImpl[]) => void;
 
   /**
    * removes a user from the dict
@@ -45,18 +45,18 @@ export interface UsersState {
 export const useUsersStore = create<UsersState>((set) => ({
   users: {},
   friendsLoaded: false,
-  addUser: (user: DiscordUserImpl) =>
+  addUser: (user: WersuUserImpl) =>
     set((state) => ({
       users: { ...state.users, [user.id]: user },
     })),
-  addFriends: (users: DiscordUserImpl[]) =>
+  addFriends: (users: WersuUserImpl[]) =>
     set((state) => ({
-      users: users.reduce<Record<string, DiscordUserImpl>>(
+      users: users.reduce<Record<string, WersuUserImpl>>(
         (acc, user) => {
           acc[user.id] = user;
           return acc;
         },
-        { ...state.users }
+        { ...state.users },
       ),
       friendsLoaded: true,
     })),
